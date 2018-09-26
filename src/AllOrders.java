246398f1a83800b53b5baaf97426cdf737b78f6a
David Aparicio <davidalejandro1223@hotmail.com>
2018-09-25 21:08:59 -0500

    añadido patron iterador interno, creados botones de obtener y editar orden falta implementar funcionalidad y agregarlos al layout de la GUI, separacion de creacion y liquidacion de ordenes hecha

---
+++ src/AllOrders.java
@@ -0,0 +1,51 @@
+
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/*this
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author david
+ */
+public class AllOrders implements Iterator{
+    private Map orders;
+    private int llave;
+    private int max;
+    private Order order;
+
+    public AllOrders() {
+        orders = new HashMap();
+        llave = 0;
+    }
+            
+    @Override
+    public boolean hasNext() {
+        if(llave == max){
+            llave =0;
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    @Override
+    public Object next() {
+       order = (Order) orders.get(llave);
+       llave++;
+       return order;
+    }
+    
+    public void put (int key, Object value){
+        max = key+1;
+        orders.put(key, value);
+    }
+    
+}
